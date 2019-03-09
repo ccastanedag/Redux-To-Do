3c6927d79840a646068f7903867b06e0fe81644e
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-03-09 00:23:55 -0500

    Organize and separate library code and app code

--- index.js
+++ index.js
@@ -1,97 +1,74 @@
-{
-  type: 'ADD_TODO',
-    todo: {
-    id: 0,
-      name: 'Learn Redux',
-        complete: false
+// Library Code
+function createStore(reducer) {
+  // The store should have 4 parts
+  // 1) The state tree (private)
+  // 2) A method to GET the state (public)
+  // 3) LISTEN to changes on the state (public)
+  // 4) UPDATE the state (public)
+  let state;
+  let listeners = [];
+
+  const getState = () => {
+
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    }
+  }
+
+  const dispatch = (action) => {
+    // call todos
+    // loop over the listeners and execute them
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
   }
 }
 
-{
-  type: 'REMOVE_TODO',
-    id: 0
-}
-
-{
-  type: 'TOGGLE_TODO',
-    id: 0
-}
-
-{
-  type: 'ADD_GOAL',
-    goal: {
-    id: 0,
-      name: 'Run a marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-    id: 0
-}
+// App Code
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
 
 function todos(state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo]); // concat don't change the original array, create another array. This keep the reducer as pure function
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id);
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map((todo) => todo.id !== action.id ? todo :
         Object.assign({}, todo, { complete: !todo.complete })
       );
     default:
       return state
   }
+}
 
-  function goals(state = [], action) {
-    switch (action.type) {
-      case 'ADD_GOAL':
-        return state.concat([action.goal])
-      case 'REMOVE_GOAL':
-        return state.filter((goal)=> goal.id !== action.id)
-      default:
-        return state
-    }
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal])
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
   }
+}
 
-  function app(state = {}, action) {
-    return {
-      todos: todos(state.todos, action),
-      goals: goals(state.goals, action)
-    }
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
   }
-
-  function createStore(reducer) {
-    // The store should have 4 parts
-    // 1) The state tree (private)
-    // 2) A method to GET the state (public)
-    // 3) LISTEN to changes on the state (public)
-    // 4) UPDATE the state (public)
-    let state;
-    let listeners = [];
-
-    const getState = () => {
-
-    }
-
-    const subscribe = (listener) => {
-      listeners.push(listener)
-      return () => {
-        listeners = listeners.filter((l) => l !== listener);
-      }
-    }
-
-    const dispatch = (action) => {
-      // call todos
-      // loop over the listeners and execute them
-      state = reducer(state, action);
-      listeners.forEach((listener) => listener());
-    }
-
-    return {
-      getState,
-      subscribe,
-      dispatch
-    }
-  }
+}
